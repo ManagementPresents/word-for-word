6bc0de673dbaa1b72e5a234dfd9ba566517f86a8
gabriel <gabriel.gonzalvez@verys.com>
2022-02-28 22:45:00 -0800

    add fool proof administration password protection

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 
 import App from './views/App';
 
+const passPrompt = prompt('enter admin password');
+
+if (passPrompt !== 'disrupt_the_planet') window.location.href = 'https://www.youtube.com/watch?v=bLlj_GeKniA';
+
 ReactDOM.render(
     <App />
 , document.getElementById('root'))
