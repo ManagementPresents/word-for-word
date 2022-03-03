824f96fc89732cd6d1455b8d111aef373d522a25
gabriel <gabriel.gonzalvez@verys.com>
2022-03-02 17:20:46 -0800

    implement ability to log in by hitting 'enter'

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,6 +1,8 @@
-import { useState } from 'react'
+import { useState, useRef } from 'react'
 import { useNavigate } from "react-router-dom";
 import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import useEventListener from "@use-it/event-listener";
+
 import { renderErrors } from '../utils/misc';
 import useStore from '../utils/store';
 
@@ -18,8 +20,20 @@ const Login = ({}: Props) => {
 	const [email, setEmail] = useState('');
 	const [password, setPassword] = useState('');
 	const [serverErrors, setServerErrors] = useState([]);
+
+	// https://atomizedobjects.com/blog/react/how-to-use-useref-with-typescript/
+	const emailInputRef = useRef<null | HTMLInputElement>(null);
+	const passwordInputRef = useRef<null | HTMLInputElement>(null);
 	
 	const navigate = useNavigate();
+
+	useEventListener('keydown', (e: KeyboardEvent) => {
+		if (e.code === 'Enter') {
+			if (document.activeElement === emailInputRef.current || document.activeElement === passwordInputRef.current) {
+				handleSignIn();
+			}
+		}
+	})
 
 	const handleSignIn = () => {
 		const auth = getAuth();
@@ -72,13 +86,17 @@ const Login = ({}: Props) => {
 
 					<div className="rounded-md shadow-sm -space-y-px">
 						<div>
+							{/* 
+								TODO: This needs to turn red when there are validation errors 
+								Also need to ensure the focus colors are correct
+							*/}
 							<label htmlFor="email-address" className="sr-only">Email address</label>
-							<input id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)}/>
+							<input ref={emailInputRef} id="email-address" name="email" type="email" autoComplete="email" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-t-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Email address" value={email} onChange={(e) => setEmail(e.target.value)}/>
 						</div>
 
 						<div>
 							<label htmlFor="password" className="sr-only">Password</label>
-							<input id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
+							<input ref={passwordInputRef} id="password" name="password" type="password" autoComplete="current-password" required className="appearance-none rounded-none relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 rounded-b-md focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)}/>
 						</div>
 					</div>
 
@@ -97,6 +115,7 @@ const Login = ({}: Props) => {
 						</div>
 					</div>
 
+					{/* TODO: Need to make sure this works when hitting 'Enter' */}
 					<div className="flex flex-col gap-y-4">
 						<Button onClick={handleSignIn} color="green" copy="Sign In" />
 						<Button onClick={() => { navigate('/register')}} color="yellow" copy="Register (It's Free!)" />
